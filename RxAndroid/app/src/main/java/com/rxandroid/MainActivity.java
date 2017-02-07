bc34cd09fcae5b66cb105692e717f01bfc77af20
Travis125 <1287200169@qq.com>
2017-02-07 21:37:08 +0800

    RxAndroid

---
+++ RxAndroid/app/src/main/java/com/rxandroid/MainActivity.java
@@ -0,0 +1,71 @@
+package com.rxandroid;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+/**
+ * Created by Travis1022 on 2017/2/7.
+ */
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick({R.id.btn_simple, R.id.btn_more, R.id.btn_lambda, R.id.btn_network, R.id.btn_safe})
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_simple:
+                SimpleModule();
+                break;
+            case R.id.btn_more:
+                MoreModule();
+                break;
+            case R.id.btn_lambda:
+                LambdaModule();
+                break;
+            case R.id.btn_network:
+                NetworkModule();
+                break;
+            case R.id.btn_safe:
+                SafeModule();
+                break;
+        }
+    }
+
+    //简单使用
+    public void SimpleModule() {
+        startActivity(new Intent(this, SimpleActivity.class));
+    }
+
+    //更多使用
+    public void MoreModule() {
+        startActivity(new Intent(this,MoreActivity.class));
+    }
+
+    //Lambda使用
+    public void LambdaModule() {
+
+    }
+
+    //网络使用
+    public void NetworkModule() {
+
+    }
+
+    //线程安全
+    public void SafeModule() {
+
+    }
+
+
+}
---
+++ RxAndroid/app/src/main/java/com/rxandroid/SimpleActivity.java
@@ -0,0 +1,89 @@
+package com.rxandroid;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import rx.Observable;
+import rx.Subscriber;
+import rx.android.schedulers.AndroidSchedulers;
+
+/**
+ * 简单的RxAndroid使用方法
+ * Created by Travis1022 on 2017/2/7.
+ */
+public class SimpleActivity extends AppCompatActivity {
+
+    @Bind(R.id.tv_simple)
+    TextView mTvSimple;
+
+    //观察事件发生
+    private Observable.OnSubscribe mOnSubscribe = new Observable.OnSubscribe<String>() {
+
+        @Override
+        public void call(Subscriber<? super String> subscriber) {
+            subscriber.onNext(sayHello());   //发送事件
+            subscriber.onCompleted();        //完成事件
+        }
+    };
+
+    //订阅者:接收字符串，显示文本
+    private Subscriber<String> mTextSubscriber = new Subscriber<String>() {
+        @Override
+        public void onCompleted() {
+
+        }
+
+        @Override
+        public void onError(Throwable e) {
+
+        }
+
+        @Override
+        public void onNext(String s) {
+            mTvSimple.setText(s);
+        }
+    };
+
+    //订阅者：接收字符串，提示信息
+    private Subscriber<String> mToastSubscriber = new Subscriber<String>() {
+        @Override
+        public void onCompleted() {
+
+        }
+
+        @Override
+        public void onError(Throwable e) {
+
+        }
+
+        @Override
+        public void onNext(String s) {
+            Toast.makeText(SimpleActivity.this, s, Toast.LENGTH_SHORT).show();
+        }
+    };
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_simple);
+        ButterKnife.bind(this);
+
+        //注册观察活动
+        Observable<String> observable = Observable.create(mOnSubscribe);
+
+        //分发订阅信息
+        observable.observeOn(AndroidSchedulers.mainThread());
+        observable.subscribe(mTextSubscriber);
+        observable.subscribe(mToastSubscriber);
+    }
+
+
+    private String sayHello() {
+        return "Hello,this is a begin of RxAndroid";
+    }
+}
