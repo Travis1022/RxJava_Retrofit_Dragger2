d947e00eb994967b525ad54f5aef800fb23e0c14
Travis125 <1287200169@qq.com>
2017-02-23 17:44:22 +0800

    activity manager

--- RxAndroid/app/src/main/java/com/rxandroid/activity/MainActivity.java
+++ RxAndroid/app/src/main/java/com/rxandroid/activity/MainActivity.java
@@ -1,8 +1,8 @@
 package com.rxandroid.activity;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
 import com.rxandroid.R;
