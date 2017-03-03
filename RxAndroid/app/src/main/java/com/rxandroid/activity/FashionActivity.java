1735ea50d352f121c8402622625107b5eca3efbc
Travis125 <1287200169@qq.com>
2017-03-03 08:33:25 +0800

    activity

---
+++ RxAndroid/app/src/main/java/com/rxandroid/activity/FashionActivity.java
@@ -0,0 +1,16 @@
+package com.rxandroid.activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * android流行框架
+ * Created by Travis on 2017/3/3.
+ */
+public class FashionActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ RxAndroid/app/src/main/java/com/rxandroid/activity/UseLibraryActivity.java
@@ -0,0 +1,16 @@
+package com.rxandroid.activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * 使用android框架
+ * Created by Travis1022 on 2017/3/3.
+ */
+public class UseLibraryActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
