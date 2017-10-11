9f8868316f125accb4acaca6a273d0cc90eac587
Travis1022 <1287200169@qq.com>
2017-10-11 17:53:18 +0800

    实时监听网络状态

--- RxAndroid/app/src/main/java/com/rxandroid/activity/BasicActivity.java
+++ RxAndroid/app/src/main/java/com/rxandroid/activity/BasicActivity.java
@@ -1,16 +1,40 @@
 package com.rxandroid.activity;
 
+import android.content.IntentFilter;
+import android.net.ConnectivityManager;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import com.rxandroid.receiver.NetWorkChangReceiver;
+
 /**
- * Activity基类
+ * Activity基类:实时获取网络状态
+ * create by Travis1022 on 2017-10-11
  */
 public class BasicActivity extends AppCompatActivity {
+    private boolean isRegistered = false;
+    private NetWorkChangReceiver netWorkChangReceiver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //注册网络状态监听广播
+        netWorkChangReceiver = new NetWorkChangReceiver();
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(WifiManager.WIFI_STATE_CHANGED_ACTION);
+        filter.addAction(WifiManager.NETWORK_STATE_CHANGED_ACTION);
+        filter.addAction(ConnectivityManager.CONNECTIVITY_ACTION);
+        registerReceiver(netWorkChangReceiver, filter);
+        isRegistered = true;
     }
 
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        //解绑
+        if (isRegistered) {
+            unregisterReceiver(netWorkChangReceiver);
+        }
+    }
 }
